0345fc52a5df3f0003d09b7231efc4861d166cda
Tobias Blomström <tobiasblomstrom@gmail.com>
2026-03-18 20:31:36 +0100

    Added nvim

---
+++ nvim/.config/nvim/lua/plugins/mason.lua
@@ -0,0 +1,9 @@
+return {
+  "mason-org/mason.nvim",
+  opts = {
+    registries = {
+      "github:Crashdummyy/mason-registry",
+      "github:mason-org/mason-registry",
+    },
+  },
+}
---
+++ nvim/.config/nvim/lua/plugins/theme.lua
@@ -0,0 +1,1 @@
+/home/tobias/.config/omarchy/current/theme/neovim.lua
